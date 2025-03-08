5e25e4fde932a2158ab2f81c99ff36c3931193ec
divyanshu1545 <daroliadivyanshu45@gmail.com>
2025-03-09 02:11:34 +0530

    reverted

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -35,7 +35,7 @@ const Links = () => {
           href={`#${item}`}
           key={item}
           variants={itemVariants}
-          whileHover={{ scale: 1.1,fontWeight:500, color:"rebeccapurple" }}
+          whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.95 }}
           whileInView={{
             transition: {
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -7,35 +7,35 @@ const skillsData = [
   {
     id: 1,
     title: "React",
-    type: "Frontend",
+    type: "frontend",
     prof: 85,
     icon: "/React-icon.svg.png",
   },
   {
     id: 2,
     title: "Flutter",
-    type: "Frontend",
+    type: "frontend",
     prof: 80,
     icon: "/Flutter_logo.svg",
   },
   {
     id: 3,
     title: "CSS",
-    type: "Frontend",
+    type: "frontend",
     prof: 70,
     icon: "/64px-CSS3_logo_and_wordmark.png",
   },
   {
     id: 4,
     title: "JavaScript",
-    type: "Frontend",
+    type: "frontend",
     prof: 90,
     icon: "/javascript.png",
   },
   {
     id: 5,
     title: "HTML",
-    type: "Frontend",
+    type: "frontend",
     prof: 80,
     icon: "/64px-HTML5_logo_and_wordmark.png",
   },
@@ -43,28 +43,28 @@ const skillsData = [
   {
     id: 6,
     title: "SpringBoot",
-    type: "Backend",
+    type: "backend",
     prof: 80,
     icon: "/Spring_Boot.png",
   },
   {
     id: 7,
     title: "MongoDB",
-    type: "Backend",
+    type: "backend",
     prof: 65,
     icon: "/Mongodb-icon.svg",
   },
   {
     id: 8,
     title: "MySQL",
-    type: "Backend",
+    type: "backend",
     prof: 85,
     icon: "/MySQL-Logo.wine.svg",
   },
   {
     id: 9,
     title: "Java",
-    type: "Backend",
+    type: "backend",
     prof: 90,
     icon: "/java.svg",
   },
@@ -131,7 +131,7 @@ function SingleSkill({ item }) {
 }
 
 function Skills() {
-  const [activeTab, setActiveTab] = useState("Backend");
+  const [activeTab, setActiveTab] = useState("backend");
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
@@ -146,20 +146,20 @@ function Skills() {
     >
       <motion.div className="tabs">
         <motion.button
-          className={activeTab === "Frontend" ? "active" : ""}
-          onClick={() => handleTabChange("Frontend")}
+          className={activeTab === "frontend" ? "active" : ""}
+          onClick={() => handleTabChange("frontend")}
         ></motion.button>
         <motion.button
           initial="initial"
           whileInView="animate"
-          className={activeTab === "Backend" ? "active" : ""}
-          onClick={() => handleTabChange("Backend")}
+          className={activeTab === "backend" ? "active" : ""}
+          onClick={() => handleTabChange("backend")}
         ></motion.button>
         <motion.button
           initial="initial"
           whileInView="animate"
-          className={activeTab === "Other" ? "active" : ""}
-          onClick={() => handleTabChange("Other")}
+          className={activeTab === "others" ? "active" : ""}
+          onClick={() => handleTabChange("others")}
         ></motion.button>
       </motion.div>
       <motion.div initial="initial" whileInView="animate" className="wrapper">
@@ -168,14 +168,8 @@ function Skills() {
           whileInView="animate"
           className="titleContainer"
         >
-          <motion.h2
-            key={activeTab}
-            initial={{ opacity: 0, y: 50,scale:0.9 }}
-            animate={{ opacity: 1, y: 0, scale:1 }}
-            transition={{ duration: 0.5 }}
-            whileInView="animate"
-          >
-            {activeTab} Skills
+          <motion.h2 initial="initialLeft" whileInView="animate">
+            Technical Skills
           </motion.h2>
         </motion.div>
         <motion.div initial="initial" whileInView="animate" className="skills">
